b3a2b83e8a77ab89922b1fe257ef7cf384d483c6
Najmeh dehghani <55862735+nmansoorabadi@users.noreply.github.com>
2019-12-05 00:42:52 -0600

    Add files via upload

--- QuickDBD-export.sql
+++ QuickDBD-export.sql
@@ -189,7 +189,7 @@ ON
 d.dep_no=de.dep_no
 WHERE d.dept_name='Sales';
 
--- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+--7- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 SELECT
 de.emp_no AS "Employee Number",
@@ -204,3 +204,12 @@ LEFT JOIN departments AS d
 ON
 d.dep_no=de.dep_no
 WHERE d.dept_name='Sales' OR d.dept_name='Development';
+
+--8- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name,COUNT(last_name)AS "Number of last_name"
+FROM employees
+GROUP BY last_name
+ORDER BY "Number of last_name" DESC;
+
+
